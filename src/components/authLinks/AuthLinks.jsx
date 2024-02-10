f04b8ecaf8e549e3928ab3afa4cfe04de7ce7a01
Subhampreet <subhampreet18@gmail.com>
2024-02-10 21:26:55 +0530

    Dev 0.0.5 : Header Development

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import styles from './authLinks.module.css';
+import Link from 'next/link';
 
 function AuthLinks() {
+  const status = "notauthenticated";
   return (
-    <div className={styles.container}>AuthLinks</div>
+    <>
+      {
+        status === "notauthenticated" ? (
+          <div className={styles.signin}>
+            <Link href="/login" >Sign in</Link>
+            <Link href="/login" className={styles.signup}>Sign up</Link>
+          </div>
+        ) : (
+          <>
+            <Link href="/write">Write</Link>
+            <span>Logout</span>
+          </>
+        )
+      }
+    </>
   )
 }
 

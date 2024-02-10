d77098a0816d0bc7d7282820f33a4b9bedcbadfa
Subhampreet <subhampreet18@gmail.com>
2024-02-10 21:58:58 +0530

    Dev 0.0.6 : Banner Development

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,9 +5,11 @@ import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
+import Banner from "@/components/banner/Banner";
 
 export default function Home() {
   return <div className={styles.container}>
+    <Banner />
     <Featured />
     <CategoryList />
     <div className={styles.content}>
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from "./themeToggle.module.css";
+import Image from 'next/image';
 
 function ThemeToggle() {
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" width={14} height={14} className={styles.sun} />   
+    </div>
   )
 }
 

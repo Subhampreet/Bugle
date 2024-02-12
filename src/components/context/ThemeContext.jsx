a6738caf11278d99a8cc698295dabf375aa89f0e
Subhampreet <subhampreet18@gmail.com>
2024-02-12 23:47:02 +0530

    Dev 0.0.8 : Context API Development

--- src/components/context/ThemeContext.jsx
+++ src/components/context/ThemeContext.jsx
@@ -1,14 +1,28 @@
-import { createContext } from "react";
+"use client";
+import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 const getFormLocalStorage = () => {
-    const value = localStorage.getItem("theme");
-    return value || "light";
+    if (typeof window === "undefined") {
+        const value = localStorage.getItem("theme");
+        return value || "light";
+    };    
 };
 
 
 export const ThemeContextProvider = ({children}) => {
-    const [theme, setTheme] = useState("light");
-    return <ThemeContext.Provider>{children}</ThemeContext.Provider>
+    const [theme, setTheme] = useState(() => {
+        return  getFormLocalStorage();
+    });
+
+    const toggle = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    }
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    return <ThemeContext.Provider value={{theme, toggle}}>{children}</ThemeContext.Provider>
 }
--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client";
+
+import React, { useContext } from 'react'
 import styles from "./themeToggle.module.css";
 import Image from 'next/image';
+import { ThemeContext } from '../context/ThemeContext';
 
 function ThemeToggle() {
+  const {toggle,theme} = useContext(ThemeContext);
+  
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <Image src="/moon.png" width={14} height={14} />
       <div className={styles.ball}></div>
       <Image src="/sun.png" width={14} height={14} className={styles.sun} />   

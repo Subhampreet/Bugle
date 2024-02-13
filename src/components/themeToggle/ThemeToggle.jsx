8206a52135199d44b2512161f88686ac78d5ac77
Subhampreet <subhampreet18@gmail.com>
2024-02-13 23:07:57 +0530

    Dev 0.0.9 : Theme Button CSS Update

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -9,9 +9,17 @@ function ThemeToggle() {
   const {toggle,theme} = useContext(ThemeContext);
   
   return (
-    <div className={styles.container} onClick={toggle}>
+    <div className={styles.container} onClick={toggle} style={
+      theme === "dark" ? 
+      { backgroundColor: "white"}
+      : { backgroundColor: "#0f172a"}
+    }>
       <Image src="/moon.png" width={14} height={14} />
-      <div className={styles.ball}></div>
+      <div className={styles.ball} style={
+        theme === "dark" ? 
+        { left : 1, backgroundColor: "#0f172a"}
+        : { right: 1, backgroundColor: "white"}
+      }></div>
       <Image src="/sun.png" width={14} height={14} className={styles.sun} />   
     </div>
   )

86a82d99eb2ab7d31fe76a1077798d0d521b8aea
Subhampreet <subhampreet18@gmail.com>
2024-02-15 21:52:52 +0530

    Dev 0.1.0 : Featured Component Development

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,9 +6,17 @@ import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 import Banner from "@/components/banner/Banner";
+import { Poppins } from "next/font/google";
+
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
 
 export default function Home() {
-  return <div className={styles.container}>
+  return <div className={`${styles.container}  ${poppins.className}`}>
     <Banner />
     <Featured />
     <CategoryList />

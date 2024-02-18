265940c6a32c7376e319461cc8e8222b76e3e55b
Subhampreet <subhampreet18@gmail.com>
2024-02-18 22:56:46 +0530

    Dev 0.1.3 : Menu Component Development

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from "./menu.module.css";
+import Link from 'next/link';
 
 function Menu() {
   return (
-    <div className={styles.container}>Menu</div>
+    <div className={styles.container}>
+      <h2>{"What's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <Link href="/" className={styles.item}></Link>
+      </div>
+    </div>
   )
 }
 
